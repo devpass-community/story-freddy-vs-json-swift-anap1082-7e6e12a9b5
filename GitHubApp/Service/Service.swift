252b568a3b90ef74fae216b734286233da6b7a6a
Ana Paula Lesniovski <anap.lesniovski@gmail.com>
2023-07-05 13:35:32 -0300

    fetchList implementando

--- GitHubApp/Service/Service.swift
+++ GitHubApp/Service/Service.swift
@@ -3,13 +3,17 @@ import Foundation
 struct Service {
     
     private let network: NetworkProtocol
+    var repositories: [Repository]?
     
     init(network: NetworkProtocol = Network()) {
         self.network = network
     }
 
     func fetchList(of user: String, completion: @escaping ([Repository]?) -> Void) {
-        
-        // TODO
+        guard let baseUrl = URL(string: "https://api.github.com/users/\(user)/repos") else { return }
+        network.performGet(url: baseUrl) { (data) in
+            completion(repositories)
+            
+        }
     }
 }
